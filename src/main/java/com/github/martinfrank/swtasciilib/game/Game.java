287c82a953d82b6349e51cda2e99595a155d1a6c
Frank Martin <Martin.Frank@finnova.com>
2021-07-06 14:35:04 +0200

    initial setup

---
+++ src/main/java/com/github/martinfrank/swtasciilib/game/Game.java
@@ -0,0 +1,14 @@
+package com.github.martinfrank.swtasciilib.game;
+
+import com.github.martinfrank.swtasciilib.resource.ResourceManager;
+
+public class Game {
+
+    private final ResourceManager resourceManager;
+    public Game(ResourceManager resourceManager) {
+        this.resourceManager = resourceManager;
+    }
+
+    public void init() {
+    }
+}
